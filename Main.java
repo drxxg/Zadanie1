0af150535fd0fce4c64823a5b3b971f2dfe80f75
Kacper Roszczyna <kacper.roszczyna@gmail.com>
2025-07-23 20:40:08 +0200

    Commented out uncompling code

--- Main.java
+++ Main.java
@@ -2,13 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Garaż auto1 = new Garaż(    "Toyota");
-        Garaż auto2 = new Garaż("Audi");
-        Garaż auto3 = new Garaż("Citroen");
-        Garaż auto4 = new Garaż("Mitsubishi");
-        Garaż auto5 = new Garaż("BMW");
-
-        System.out.println("W garażu znajduje sie " + Garaż.LiczbaAut + " samochodów");
+//        Garaż auto1 = new Garaż(    "Toyota");
+//        Garaż auto2 = new Garaż("Audi");
+//        Garaż auto3 = new Garaż("Citroen");
+//        Garaż auto4 = new Garaż("Mitsubishi");
+//        Garaż auto5 = new Garaż("BMW");
+//
+//        System.out.println("W garażu znajduje sie " + Garaż.LiczbaAut + " samochodów");
 
     }
 
